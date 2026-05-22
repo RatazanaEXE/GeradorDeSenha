deec557b1d2f9858cfeba152158fc49403c4f885
RatazanaEXE <claudia.r.carvalho2015@gmail.com>
2026-05-22 13:13:59 -0300

    Inserção de novos caracteres para gerar senhas mais seguras

--- src/main/java/GeradorDeSenha.java
+++ src/main/java/GeradorDeSenha.java
@@ -2,38 +2,53 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.security.SecureRandom;
-
 
 public class GeradorDeSenha extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
-    public String gerarSenha() {
+
+    final static String caracteres =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*()-_=+[]{}<>?";
+
+    public String gerarSenha(int comprimento) {
 
         SecureRandom aleatorio = new SecureRandom();
+        StringBuilder senha = new StringBuilder(comprimento);
 
-        String senha ="";
-        for (int i = 0; i < 8; i++) {
-            senha += aleatorio.nextInt(10);
+        for (int i = 0; i < comprimento; i++) {
+            int indice = aleatorio.nextInt(caracteres.length());
+            senha.append(caracteres.charAt(indice));
         }
-        return senha;
+
+        return senha.toString();
     }
+
     @Override
     public void start(Stage palco) {
 
-        Label etiquetaSenha = new Label("Clique para gerar");
+        Label etiquetaSenha = new Label("Digite o comprimento da senha");
+
+        TextField campoComprimento = new TextField();
+        campoComprimento.setPromptText("Digite o comprimento");
+
         Button gerarSenha = new Button("Gerar senha");
 
         gerarSenha.setOnAction(e -> {
-            String senha = String.valueOf(gerarSenha());
+
+            int comprimento = Integer.parseInt(campoComprimento.getText());
+
+            String senha = gerarSenha(comprimento);
+
             etiquetaSenha.setText(senha);
         });
 
-        VBox layout = new VBox(etiquetaSenha, gerarSenha);
+        VBox layout = new VBox(10, etiquetaSenha, campoComprimento, gerarSenha);
 
         Scene cena = new Scene(layout, 300, 200);
 
